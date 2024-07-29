6d4dc1a9f66bc1765e01d826b80a8825d7fb0d1e
mgoyeghiaian <mgoyeghiaian@gmail.com>
2024-07-29 09:53:19 +0300

    Nothing

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import services from '/src/Db/ServicesDb.js';
